99a31b691235defc4c6b6fa280479867acc00cc9
Kost0 <lavrenov.konstantin@gmail.com>
2025-08-02 17:29:44 +0300

    Add functions for work with db and grpc

--- internal/repository/productPostgres.go
+++ internal/repository/productPostgres.go
@@ -31,3 +31,88 @@ func (r *ProductRepository) GetByID(ctx context.Context, id string) (*domain.Pro
 
 	return &product, nil
 }
+
+func (r *ProductRepository) GetAll(ctx context.Context) ([]domain.Product, error) {
+	const query = `SELECT * FROM products;`
+
+	var products []domain.Product
+
+	rows, err := r.db.QueryContext(ctx, query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var product domain.Product
+		err = rows.Scan(&product.ID, &product.Name)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, product)
+	}
+
+	err = rows.Err()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
+
+func (r *ProductRepository) Create(ctx context.Context, product *domain.Product) (*domain.Product, error) {
+	const query = `
+INSERT INTO products (name, description, priceBuy, priceSell, supplierId, weight)
+VALUES ($1, $2, $3, $4, $5, $6);
+`
+	err := r.db.QueryRowContext(ctx, query,
+		product.Name,
+		product.Description,
+		product.PriceBuy,
+		product.PriceSell,
+		product.SupplierID,
+		product.Weight).Scan(&product.ID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (r *ProductRepository) Update(ctx context.Context, product *domain.Product) error {
+	const query = `
+UPDATE products
+SET name = $1, description = $2, priceBuy = $3, priceSell = $4, supplierId = $5, weight = $6
+WHERE id = $7;
+`
+
+	_, err := r.db.ExecContext(ctx, query,
+		product.Name,
+		product.Description,
+		product.PriceBuy,
+		product.PriceSell,
+		product.SupplierID,
+		product.Weight,
+		product.ID)
+
+	return err
+}
+
+func (r *ProductRepository) Delete(ctx context.Context, id string) error {
+	const query = `DELETE FROM products WHERE id = $1;`
+
+	_, err := r.db.ExecContext(ctx, query, id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- internal/service/product.go
+++ internal/service/product.go
@@ -1,11 +1,85 @@
 package service
 
 import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
 	pb "products/gen/go/product"
+	"products/internal/domain"
 	"products/internal/repository"
+	"strconv"
 )
 
 type ProductServer struct {
 	pb.ProductServiceServer
 	repo repository.ProductRepository
+}
+
+func NewProductServer(repo repository.ProductRepository) *ProductServer {
+	return &ProductServer{repo: repo}
+}
+
+func (s *ProductServer) CreateProduct(ctx context.Context, req *pb.CreateProductRequest) (*pb.ProductResponse, error) {
+	if req.Name == "" {
+		return nil, status.Error(codes.InvalidArgument, "name is required")
+	}
+	if req.Description == "" {
+		return nil, status.Error(codes.InvalidArgument, "description is required")
+	}
+	if req.PriceBuy < 0 || req.PriceSell < 0 {
+		return nil, status.Error(codes.InvalidArgument, "price_buy or price_sell less then zero")
+	}
+	if req.SupplierId < 0 {
+		return nil, status.Error(codes.InvalidArgument, "supplier_id is required")
+	}
+	if req.Weight < 0 {
+		return nil, status.Error(codes.InvalidArgument, "weight is required")
+	}
+
+	product := &domain.Product{
+		Name:        req.Name,
+		Description: req.Description,
+		PriceBuy:    int(req.PriceBuy),
+		PriceSell:   int(req.PriceSell),
+		SupplierID:  int(req.SupplierId),
+		Weight:      int(req.Weight),
+	}
+
+	createdProduct, err := s.repo.Create(ctx, product)
+	if err != nil {
+		log.Printf("CreateProduct failed: %v", err)
+		return nil, status.Error(codes.Internal, "CreateProduct failed")
+	}
+
+	return &pb.ProductResponse{
+		ProductId:   strconv.Itoa(createdProduct.ID),
+		Name:        createdProduct.Name,
+		Description: createdProduct.Description,
+		PriceBuy:    int32(createdProduct.PriceBuy),
+		PriceSell:   int32(createdProduct.PriceSell),
+		SupplierId:  int32(createdProduct.SupplierID),
+		Weight:      int32(createdProduct.Weight),
+	}, nil
+}
+
+func (s *ProductServer) GetProduct(ctx context.Context, req *pb.GetProductRequest) (*pb.ProductResponse, error) {
+	receivedProduct, err := s.repo.GetByID(ctx, req.ProductId)
+	if err != nil {
+		log.Printf("GetProduct failed: %v", err)
+		return nil, status.Error(codes.Internal, "Error")
+	}
+	if receivedProduct == nil {
+		return nil, status.Error(codes.NotFound, "Product not found")
+	}
+
+	return &pb.ProductResponse{
+		ProductId:   strconv.Itoa(receivedProduct.ID),
+		Name:        receivedProduct.Name,
+		Description: receivedProduct.Description,
+		PriceBuy:    int32(receivedProduct.PriceBuy),
+		PriceSell:   int32(receivedProduct.PriceSell),
+		SupplierId:  int32(receivedProduct.SupplierID),
+		Weight:      int32(receivedProduct.Weight),
+	}, nil
 }
